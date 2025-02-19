a857833fa19f0f9ddc70de630bfe4e624f455506
Carlos Delgado <carlos.dz16180@gmail.com>
2025-02-19 14:32:02 +0100

    Estructura clase movimiento

--- src/Movement.java
+++ src/Movement.java
@@ -1,3 +1,14 @@
 public class Movement {
-    
+    public String name;
+    public int id;
+    public String type;
+    public String family;
+    public int power;
+    public int accuracy;
+    public int PP;
+
+    public int cur_PP;
+
+    public boolean disabled;
+    public boolean encored;
 }
