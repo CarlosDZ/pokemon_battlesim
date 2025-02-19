d4495e216b40eddd973ab507a43238053e39d5c7
Carlos Delgado <carlos.dz16180@gmail.com>
2025-02-19 15:57:34 +0100

    Creacion y atributos de la clase Team

---
+++ src/Team.java
@@ -0,0 +1,15 @@
+public class Team {
+    public int Player;
+
+    public Pokemon Pokemon1;
+    public Pokemon Pokemon2;
+    public Pokemon Pokemon3;
+    public Pokemon Pokemon4;
+    public Pokemon Pokemon5;
+    public Pokemon Pokemon6;
+
+    public boolean hasLost = false;
+
+    
+
+}
