0f26544f0fdac91ea7e88a975569886396750260
Carlos Delgado <carlos.dz16180@gmail.com>
2025-02-19 10:26:34 +0100

    Programacion de los metodos getFamily, getPow y getAccur

--- src/MoveReader.java
+++ src/MoveReader.java
@@ -41,7 +41,7 @@ public class MoveReader {
             while((line = reader.readLine()) != null){
                 Matcher matcher = pattern.matcher(line);
                 if(matcher.find()) {
-                    Pattern typePattern = Pattern.compile("\\s*[A-Z]+:([A-Za-z]+)");
+                    Pattern typePattern = Pattern.compile("\\s*TYPE:([A-Za-z]+)");
                     while((line = reader.readLine()) != null){
                         Matcher typeMatcher = typePattern.matcher(line);
                         if(typeMatcher.find()){
@@ -57,5 +57,86 @@ public class MoveReader {
             e.printStackTrace();
         }
         return "-1";
+    }
+
+    public String getFamily(int moveID){
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+
+            Pattern pattern = Pattern.compile("\\s*[A-Za-z0-9 -]+\\{ID:"+moveID+"\\s*");
+            
+            String line;
+            while((line = reader.readLine()) != null){
+                Matcher matcher = pattern.matcher(line);
+                if(matcher.find()) {
+                    Pattern famPattern = Pattern.compile("\\s*FAMILY:([A-Za-z]+)");
+                    while((line = reader.readLine()) != null){
+                        Matcher famMatcher = famPattern.matcher(line);
+                        if(famMatcher.find()){
+                            String type = famMatcher.group(1);
+                            return type;
+                        }
+                    }
+                    
+                }
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return "-1";
+    }
+
+    public int getPower(int moveID){
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+
+            Pattern pattern = Pattern.compile("\\s*[A-Za-z0-9 -]+\\{ID:"+moveID+"\\s*");
+            
+            String line;
+            while((line = reader.readLine()) != null){
+                Matcher matcher = pattern.matcher(line);
+                if(matcher.find()) {
+                    Pattern powPattern = Pattern.compile("\\s*POWER:(\\d*)");
+                    while((line = reader.readLine()) != null){
+                        Matcher powMatcher = powPattern.matcher(line);
+                        if(powMatcher.find()){
+                            int pow = Integer.parseInt(powMatcher.group(1));
+                            return pow;
+                        }
+                    }
+                    
+                }
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public int getAccur(int moveID){
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+
+            Pattern pattern = Pattern.compile("\\s*[A-Za-z0-9 -]+\\{ID:"+moveID+"\\s*");
+            
+            String line;
+            while((line = reader.readLine()) != null){
+                Matcher matcher = pattern.matcher(line);
+                if(matcher.find()) {
+                    Pattern accPattern = Pattern.compile("\\s*ACCUR:(\\d*)");
+                    while((line = reader.readLine()) != null){
+                        Matcher accMatcher = accPattern.matcher(line);
+                        if(accMatcher.find()){
+                            int acc = Integer.parseInt(accMatcher.group(1));
+                            return acc;
+                        }
+                    }
+                    
+                }
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
